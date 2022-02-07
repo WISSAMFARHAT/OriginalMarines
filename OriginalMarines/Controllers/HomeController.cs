b5fc7883eae036f3b50691a503d19b5c67754a25
farha <farha@DESKTOP-8DE9EKJ>
2022-02-07 17:35:02 +0200

    Web app Ecommerce OriginalMarines

---
+++ OriginalMarines/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OriginalMarines.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult category(string name)
+        {
+            ViewBag.name = name;
+            return View();
+        }
+
+        public ActionResult item()
+        {
+            
+            return View();
+        }
+
+        public ActionResult cart()
+        {
+
+            return View();
+        }
+
+        public ActionResult whishlist()
+        {
+
+            return View();
+        }
+
+        public ActionResult checkout()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ OriginalMarines/Scripts/nav.js
@@ -0,0 +1,46 @@
+﻿$(window).scroll(function () {
+    if ($(window).scrollTop() > document.getElementById('topbar').offsetHeight) {
+        document.getElementById("nav").style.backgroundColor = "white";
+        document.getElementById("nav").style.position = "fixed";
+        document.getElementById("nav").style.top = "0";
+        $(".nav-link").each(function () {
+            $(this).addClass("black");
+            
+        });
+
+        document.getElementById("down").style.display = "block";
+        
+    } else {
+        document.getElementById("nav").style.backgroundColor = "transparent";
+        document.getElementById("nav").style.position = "relative";
+        $(".nav-link").each(function () {
+            $(this).removeClass("black");
+            
+        });
+        document.getElementById("down").style.display = "none";
+
+    }
+});
+
+function down()
+{
+    window.scrollTo(0, 0);
+}
+
+
+
+$(document).ready(function () {
+    $('.count').prop('disabled', true);
+    $(document).on('click', '.plus', function () {
+        $('.count').val(parseInt($('.count').val()) + 1);
+    });
+    $(document).on('click', '.minus', function () {
+        $('.count').val(parseInt($('.count').val()) - 1);
+        if ($('.count').val() == 0) {
+            $('.count').val(1);
+        }
+    });
+    
+});
+
+
